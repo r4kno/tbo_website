565f827be2e084f0effb8e9f0ee72b60cb64e1f7
Onkar Gupta <onkargupta0864@gmail.com>
2025-01-15 17:49:19 +0530

    react app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import Header from './components/Header'
+// import Hero from './components/Hero'
+// import SearchForm from './components/SearchForm'
+// import Brief from './components/Brief'
+// import Timeline from './components/Timeline'
+// import FlightCard from './components/FlightCard'
+
+// export default function Home() {
+//   return (
+//     <div className="bg-red-800">
+//       ghgdcjgsd
+//     </div>
+//   )
+// }
+
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-red-800'>App</div>
   )
 }
 
